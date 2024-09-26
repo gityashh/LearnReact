1f6862153370731ba3dee2f7da4a6d571573f069
gityashh <yashrajput8359@gmail.com>
2024-09-26 17:29:06 +0530

    two way binding initial

--- README.md
+++ README.md
@@ -13,10 +13,20 @@ Currently, two official plugins are available:
 2. Run `npm install`
 3. Run `npm run dev`
 
-//26/09/2024-4:30
+//25/09/2024-4:30
 
 # What is the project?
 
 This is a simple login form that allows users to login with their username and password with the help of react we are learning how to handle form submission and how to send data to the server without refreshing the page.
 
+
+//26/09/2024-4:30
+
+# Two way binding in react
+
+Two way binding is a feature in react that allows us to bind the value of a form input to a state variable. This means that when the value of the input changes, the state variable will also change, and vice versa.
+
+# How to do two way binding in react
+
+We can do two way binding in react by using the useState hook. The useState hook is a hook that allows us to create a state variable.
 
--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const App = () => {
 
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    const formData = new FormData(e.target);  
-    console.log(formData);
-    // send data to server ->
+    console.log({
+      username,
+      password
+    });
+  }
+
+  const handleChange = (e) => {
+    setUsername(e.target.value);
+    console.log(e.target.value);
   }
 
   return (
     <div className='flex flex-col items-center justify-center h-screen'>
       <form className='flex flex-col gap-4' action="" onSubmit={handleSubmit}>
-      <input className='border-2 border-gray-300 rounded-md p-2' type="text" name="username" placeholder="Username" />
-      <input className='border-2 border-gray-300 rounded-md p-2' type="password" name="password" placeholder="Password"  />
+      <input className='border-2 border-gray-300 rounded-md p-2' type="text" name="username" placeholder="Username" value={username} onChange={handleChange} />
+      <input className='border-2 border-gray-300 rounded-md p-2' type="password" name="password" placeholder="Password" value={password} onChange={handleChange}  />
       <button className='bg-blue-500 text-white p-2 rounded-md' type="submit">Submit</button>
     </form>
     </div>

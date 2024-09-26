3b74fd1806f307b77ee661b4ac3fd866ee995453
gityashh <yashrajput8359@gmail.com>
2024-09-26 17:08:49 +0530

    FormData object

--- README.md
+++ README.md
@@ -6,3 +6,17 @@ Currently, two official plugins are available:
 
 - [@vitejs/plugin-react](https://github.com/vitejs/vite-plugin-react/blob/main/packages/plugin-react/README.md) uses [Babel](https://babeljs.io/) for Fast Refresh
 - [@vitejs/plugin-react-swc](https://github.com/vitejs/vite-plugin-react-swc) uses [SWC](https://swc.rs/) for Fast Refresh
+
+# How to run the project
+
+1. Clone the repository
+2. Run `npm install`
+3. Run `npm run dev`
+
+//26/09/2024-4:30
+
+# What is the project?
+
+This is a simple login form that allows users to login with their username and password with the help of react we are learning how to handle form submission and how to send data to the server without refreshing the page.
+
+
--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
 const App = () => {
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);  
+    console.log(formData);
+    // send data to server ->
+  }
+
   return (
-    <div>App</div>
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <form className='flex flex-col gap-4' action="" onSubmit={handleSubmit}>
+      <input className='border-2 border-gray-300 rounded-md p-2' type="text" name="username" placeholder="Username" />
+      <input className='border-2 border-gray-300 rounded-md p-2' type="password" name="password" placeholder="Password"  />
+      <button className='bg-blue-500 text-white p-2 rounded-md' type="submit">Submit</button>
+    </form>
+    </div>
   )
 }
 

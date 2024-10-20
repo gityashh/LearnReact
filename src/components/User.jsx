c4694f4e495f22ea4adf250e9811d83ddce1a3a1
gityashh <yashrajput8359@gmail.com>
2024-10-20 13:26:34 +0530

    users route ready

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState} from 'react';
+import { axiosInstance } from "../utils/axios"
 
 const User = () => {
+    const getUser = async () => {
+        const res = await axiosInstance.get("/users")
+        console.log(res.data);
+    }
   return (
-    <div>User</div>
-  )
-}
+    <div className='w-full h-[80vh] flex flex-col items-center justify-center gap-4'>
+      <h1>User</h1>
+      <button className='bg-blue-500 text-white px-4 py-2 rounded' onClick={getUser}>Get User</button>
+    </div>
+  );
+};
 
-export default User
+export default User;

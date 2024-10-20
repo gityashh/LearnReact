d3753318429f128f5b78c4738b42ed4f38866471
gityashh <yashrajput8359@gmail.com>
2024-10-20 13:25:06 +0530

    products route ready

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React, { useState} from 'react';
+import { axiosInstance } from "../utils/axios"
 
 const Products = () => {
+    const getProducts = async () => {
+        const res = await axiosInstance.get("/products")
+        console.log(res.data);
+    }
   return (
-    <div>Products</div>
-  )
-}
+    <div className='w-full h-[80vh] flex flex-col items-center justify-center gap-4'>
+      <h1>Products</h1>
+      <button className='bg-blue-500 text-white px-4 py-2 rounded' onClick={getProducts}>Get products</button>
+    </div>
+  );
+};
 
-export default Products
+export default Products;

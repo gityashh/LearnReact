802e14be700dc98332c4fec8a4809fa209b100ad
gityashh <yashrajput8359@gmail.com>
2024-10-20 15:54:12 +0530

    basic useEffect hook use

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 const Home = () => {
+    useEffect(() => {
+        console.log("Home mounted");      
+    })
   return (
-    <div>Home</div>
+    <div></div>
   )
 }
 

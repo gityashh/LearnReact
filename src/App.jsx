94f89323fc7d78c60338eb9bffd446bec5e759cf
gityashh <yashrajput8359@gmail.com>
2024-10-20 16:21:01 +0530

    home.jsx re-mounting

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Home from './components/Home'
-import Products from './components/Products'
-import User from './components/User'
-import Layout from './components/Layout'
+import React from 'react';
+import Nav from './components/Nav';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Products from './components/Products';
+import User from './components/User';
+import Layout from './components/Layout';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path='products' element={<Products />} />
-        <Route path='users' element={<User />} />
-        <Route path='*' element={<h1>404 Not Found</h1>} />
-      </Route>
-    </Routes>
-  )
-}
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/users" element={<User />} />
+          <Route path="*" element={<h1>404 Not Found</h1>} />
+        </Route>
+      </Routes>
+  );
+};
 
-export default App
+export default App;

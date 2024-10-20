3abbab89ffd88b2dbe855ea7bd53a2c9bf34e754
gityashh <yashrajput8359@gmail.com>
2024-10-20 12:55:13 +0530

    basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,8 @@
-// import React from 'react';
-// import Child2 from './components/Child2';
-// import Child3 from './components/Child3';
-// import Child4 from './components/Child4';
-
-
-// // Basic prop use
-
-// const App = () => {
-//   const greeting = "hello"
-//   return (
-//     <div>
-//       <Child2 title={greeting} />
-//       <Child3 profile={{name:"yash", age:"22"} } greeting='afternoon' />
-//     </div>
-//   );
-// };
-
-// // Prop validation
-
-// // const App = () => {
-// //   const name = "yash"
-// //   const age = 22;
-// //   const hobbies = ["cricket", "football"];
-  
-// //   return (
-// //     <div>
-// //       <Child4 hobbies={hobbies} name={name} age={age} isLoggedIn={ true } />
-// //     </div>
-// //   );
-// // };
-
-
-// export default App;
-
 import React from 'react'
-import Container from './components/Container'
 
 const App = () => {
   return (
-    <Container >
-      <h1 name="greeting">Hello</h1>
-      <h2 name="hello">World</h2>
-    </Container>
+    <div>App</div>
   )
 }
 
--- src/components/Child2.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Child2 = (props) => {
-  return (
-      <div>{props.title}</div>
-  )
-}
-
-export default Child2

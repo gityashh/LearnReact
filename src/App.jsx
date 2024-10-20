c09472343fd67da6646c8f76786c01fce3e3bdc2
gityashh <yashrajput8359@gmail.com>
2024-10-20 13:12:48 +0530

    folder structure ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import Nav from './components/Nav'
+import { Route, Routes } from 'react-router-dom'
+import Home from './components/Home'
+import Products from './components/Products'
+import User from './components/User'
+import Cart from './components/Cart'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='w-full min-h-screen'>
+      <Nav />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/products' element={<Products/>} />
+        <Route path='/users' element={<User/>} />
+        <Route path='/cart' element={<Cart/>}/>
+      </Routes>
+    </div>
   )
 }
 

77ce2c91a51cb1a1d1640653ec4836377828126e
gityashh <yashrajput8359@gmail.com>
2024-10-20 16:10:07 +0530

    learning outlet

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
 import React from 'react'
-import Nav from './components/Nav'
 import { Route, Routes } from 'react-router-dom'
 import Home from './components/Home'
 import Products from './components/Products'
 import User from './components/User'
+import Layout from './components/Layout'
 
 const App = () => {
   return (
-    <div className='w-full min-h-screen flex flex-col'>
-      <Nav />
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/products' element={<Products/>} />
-        <Route path='/users' element={<User />} />
-        <Route path='/*' element={<h1>404 Not Found</h1>} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path='products' element={<Products />} />
+        <Route path='users' element={<User />} />
+        <Route path='*' element={<h1>404 Not Found</h1>} />
+      </Route>
+    </Routes>
   )
 }
 

d5fb92c7f4d93437ca9ef3baba2a80d70c5d0daa
gityashh <yashrajput8359@gmail.com>
2024-10-23 16:45:59 +0530

    ready

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,7 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
-import NewContext from './contexts/NewContext.jsx';
 
 createRoot(document.getElementById('root')).render(
-    <NewContext>
-        <App />
-    </NewContext>
+    <App />
 );

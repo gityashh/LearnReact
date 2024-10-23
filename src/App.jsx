6c02a35855ecbbe3641d1bed34fc3c6330fbf05a
gityashh <yashrajput8359@gmail.com>
2024-10-23 23:56:38 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -11,11 +11,15 @@ const App = () => {
     dispatch(decrement());
   }
   return (
-    <div>
-      counter:{count}
-      <button onClick={increHandler}>Increment by 1</button>
-      <button onClick={decreHandler}>Decrement by 1</button>
-      <button onClick={() => dispatch(asyncIncrement())}>Async Increment</button>
+    <div className='flex items-center justify-center h-screen'>
+      <div className='flex flex-col items-center justify-center gap-4 bg-zinc-900 text-white rounded w-80 px-12 py-4'>
+      <h1 className='font-thin text-2xl bg-zinc-700 px-12 py-6 rounded'>counter:{count}</h1>
+      <div className='flex gap-4'>
+      <button className='bg-zinc-500 px-4 py-2 rounded text-xs font-semibold' onClick={increHandler}>Increment by 1</button>
+      <button className='bg-zinc-500 px-4 py-2 rounded text-xs font-semibold' onClick={decreHandler}>Decrement by 1</button>
+      <button className='bg-zinc-500 px-4 py-2 rounded text-xs font-semibold' onClick={() => dispatch(asyncIncrement())}>Async Increment</button>
+      </div>
+    </div>
     </div>
   )
 }

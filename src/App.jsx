e070c01955f39dbda7e44b85fad412f2bdad4609
gityashh <yashrajput8359@gmail.com>
2024-10-25 00:29:21 +0530

    new changes

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,24 @@ const App = () => {
   const { register,
     handleSubmit
    } = useForm();
+
+   const onSubmit = (data) => {
+    console.log(data);
+   }
   return (
     <div className="h-screen w-full flex items-center justify-center bg-zinc-100 flex-col">
-      <form action="" className="flex flex-col gap-2 items-start">
+      <form action="" className="flex flex-col gap-2 items-start" onSubmit={handleSubmit(onSubmit)}>
         <input
           type="text"
           placeholder="username"
           className="input-field font-thin px-3 py-1 border border-black-300 outline-none tracking-widest text-zinc-500 rounded-md"
+          {...register("username")}
         />
         <input
           type="text"
           placeholder="password"
           className="input-field font-thin px-3 py-1 border border-black-300  outline-none tracking-widest text-zinc-500 rounded-md"
+          {...register("password")}
         />
         <button className="px-3 py-1 rounded-md bg-teal-500 text-white">
           Sign up

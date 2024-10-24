f8f2d06f724d8d8ffbe660a67360adae5b16a861
gityashh <yashrajput8359@gmail.com>
2024-10-24 13:53:31 +0530

    todo app without UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,22 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { asyncIncrement, decrement, increment } from './store/Actions/action';
+import React from 'react';
+import { useSelector } from 'react-redux';
+import TodoList from './components/todoComponent';
+import Auth from './components/authComponent';
 
-const App = () => {
-  const count = useSelector((state) => state.counter.value)
-  const dispatch = useDispatch();
-  const increHandler = () => {
-    dispatch(increment());
-  }
-  const decreHandler = () => {
-    dispatch(decrement());
-  }
+function App() {
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated);
+
   return (
-    <div className='flex items-center justify-center h-screen'>
-      <div className='flex flex-col items-center justify-center gap-4 bg-zinc-900 text-white rounded w-80 px-12 py-4'>
-      <h1 className='font-thin text-2xl bg-zinc-700 px-12 py-6 rounded'>counter:{count}</h1>
-      <div className='flex gap-4'>
-      <button className='bg-zinc-500 px-4 py-2 rounded text-xs font-semibold' onClick={increHandler}>Increment by 1</button>
-      <button className='bg-zinc-500 px-4 py-2 rounded text-xs font-semibold' onClick={decreHandler}>Decrement by 1</button>
-      <button className='bg-zinc-500 px-4 py-2 rounded text-xs font-semibold' onClick={() => dispatch(asyncIncrement())}>Async Increment</button>
-      </div>
+    <div className="App">
+      <h1>Redux Toolkit App</h1>
+      <Auth />
+      {isAuthenticated && (
+        <>
+          <TodoList />
+        </>
+      )}
     </div>
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;

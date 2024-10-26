509d3e8d597198a3291520cee0b9b6c2cc97a067
gityashh <yashrajput8359@gmail.com>
2024-10-26 17:17:21 +0530

    memoization of a component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import React, { useState, useMemo } from 'react'
+import Nav from './Nav';
 
 
 const App = () => {
   const [count, setcount] = useState(0)
+  const [first, setfirst] = useState("Navdata")
 
   // const bigfunc = () => {
   //   for (let index = 0; index < 1000000000000000; index++) {
@@ -10,15 +12,12 @@ const App = () => {
   //   return count;
   // }
 
-  
-
-  const memoizedBigFunc = useMemo(bigfunc, [count]);
+  // const memoizedBigFunc = useMemo(bigfunc, [count]);
 
   return (
     <div>
-      {memoizedBigFunc}
-      <h1>{count}</h1>
-      <button onClick={()=>setcount(count+1)}>click</button>
+      <Nav dets={first} />
+      <button onClick={()=>setfirst(`new data ${Math.random()}`)}>click</button>
     </div>
 
   )
---
+++ src/Nav.jsx
@@ -0,0 +1,10 @@
+import React, { memo } from 'react'
+
+const Nav = (props) => {
+    console.log("nav rendered");
+  return (
+      <div>{ props.dets }</div>
+  )
+}
+
+export default memo(Nav)

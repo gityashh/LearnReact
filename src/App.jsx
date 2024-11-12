0d9fbcaec35539a6074a3a55b02645445279ca23
gityashh <yashrajput8359@gmail.com>
2024-11-12 11:28:44 +0530

    validation added

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,12 @@ import { useForm } from 'react-hook-form';
 
 
 const App = () => {
-  const { register,
-    handleSubmit
-   } = useForm();
+  const { register, handleSubmit, formState: { errors } } = useForm();
 
-   const onSubmit = (data) => {
+  const onSubmit = (data) => {
     console.log(data);
-   }
+  }
+
   return (
     <div className="h-screen w-full flex items-center justify-center bg-zinc-100 flex-col">
       <form action="" className="flex flex-col gap-2 items-start" onSubmit={handleSubmit(onSubmit)}>
@@ -17,14 +16,18 @@ const App = () => {
           type="text"
           placeholder="username"
           className="input-field font-thin px-3 py-1 border border-black-300 outline-none tracking-widest text-zinc-500 rounded-md"
-          {...register("username")}
+          {...register("username", { required: "Username is required" })}
         />
+        {errors.username && <span className="text-red-500">{errors.username.message}</span>}
+        
         <input
           type="text"
           placeholder="password"
-          className="input-field font-thin px-3 py-1 border border-black-300  outline-none tracking-widest text-zinc-500 rounded-md"
-          {...register("password")}
+          className="input-field font-thin px-3 py-1 border border-black-300 outline-none tracking-widest text-zinc-500 rounded-md"
+          {...register("password", { required: "Password is required", minLength: { value: 6, message: "Password must be at least 6 characters" } })}
         />
+        {errors.password && <span className="text-red-500">{errors.password.message}</span>}
+        
         <button className="px-3 py-1 rounded-md bg-teal-500 text-white">
           Sign up
         </button>
